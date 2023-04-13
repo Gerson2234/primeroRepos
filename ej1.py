75168633f1ddc04a64477a5ec5fb18886751e82e
Gerson Renato Yramategui Lozano <gerson4832@gmail.com>
2023-04-13 17:43:25 -0500

    [ADD] Scripts de python de ejemplo

---
+++ ej1.py
@@ -0,0 +1,6 @@
+print("mensaje 1 de python")
+print("mensaje 2 de python")
+a = 64
+b = 15
+c = a + b
+print(str(c))
---
+++ ej2.py
@@ -0,0 +1,14 @@
+nombre = "Gerson"
+edad = 29
+if (len(nombre) < 9):
+    #set de instrucciones A
+    print("La condicion es verdadera")
+    a = 5
+    b = 10
+    print(str(a+b))
+else:
+    #set de instrucciones B
+    print("La condicion es falsa")
+    a = 20
+    b = 30
+    print(str(a+b))
